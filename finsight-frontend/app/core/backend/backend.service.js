df6fd68800e2008b0354c15502d1388950366236
mikouaj <nick@surreal.pl>
2016-11-28 11:31:06 +0100

    Fixed Backend getApi promise bug

--- finsight-frontend/app/core/backend/backend.service.js
+++ finsight-frontend/app/core/backend/backend.service.js
@@ -5,9 +5,9 @@ angular.
   factory('Backend', ['$http','AngularSwaggerific',
     function($http,AngularSwaggerific) {
     	var getApi = function() {
-    		$http.get('http://petstore.swagger.io/v2/swagger.json').then(function(response) {
-    			return new AngularSwaggerific(response.data);
-    		});
+        return $http.get('http://petstore.swagger.io/v2/swagger.json').then(function(response) {
+          return new AngularSwaggerific(response.data);
+        });
     	}
     	return {
     		getApi: getApi
