768044d853f708096acd1ff5569d1261ff9c0da8
mikouaj <nick@surreal.pl>
2016-12-06 14:44:16 +0100

    Backend service is not fetching swagger file on every call of getApi

--- finsight-frontend/app/core/backend/backend.service.js
+++ finsight-frontend/app/core/backend/backend.service.js
@@ -2,12 +2,20 @@
 
 angular.
   module('core.backend').
-  factory('Backend', ['$http','AngularSwaggerific',
-    function($http,AngularSwaggerific) {
+  factory('Backend', ['$http','$q','AngularSwaggerific',
+    function($http,$q,AngularSwaggerific) {
+      var self = this;
     	var getApi = function() {
-        return $http.get('http://127.0.0.1:8090/api/swagger.json').then(function(response) {
-          return new AngularSwaggerific(response.data);
-        });
+        var deferred = $q.defer();
+        if(!self.swaggerific) {
+          $http.get('http://127.0.0.1:8090/api/swagger.json').then(function(response) {
+            self.swaggerific = new AngularSwaggerific(response.data);
+            deferred.resolve(self.swaggerific);
+          });
+        } else {
+          deferred.resolve(self.swaggerific);
+        }
+        return deferred.promise;
     	}
       var getTransactionImportURL = function() {
         return 'http://127.0.0.1:8090/api/transactions/import';
