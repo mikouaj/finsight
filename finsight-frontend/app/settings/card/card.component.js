6b160c6846208d37ee6498f1b9790273fceffbf2
mikouaj <nick@surreal.pl>
2016-12-09 16:50:55 +0100

    Card inline edit added, started dele operation

--- finsight-frontend/app/settings/card/card.component.js
+++ finsight-frontend/app/settings/card/card.component.js
@@ -6,10 +6,17 @@ angular.
     templateUrl: 'settings/card/card.template.html',
     controller: ['Backend',function CardController(Backend) {
       var self = this;
+      self.cards = [];
       Backend.getApi().then(function(api) {
       	api.cards.getCards().then(function(response) {
       		self.cards = response.data;
       	});
       });
+
+      self.removeCard = function(idx,card) {
+        Backend.getApi().then(function(api) {
+          api.cards.delete({cardId:card.id});
+        });
+      }
     }]
   });
