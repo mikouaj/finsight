9d03c451939d0357fccb526ad1ff13733367ea8c
mikouaj <nick@surreal.pl>
2017-01-11 14:47:51 +0100

    Added footer with "scrtoll to top" link that is present only when scrollbar appears (long page)

--- finsight-frontend/app/app.controller.js
+++ finsight-frontend/app/app.controller.js
@@ -6,4 +6,26 @@ angular.
     $scope.isActive = function (viewLocation) {
       return viewLocation === $location.path(); 
     }
-  }]);
+  }]).
+  controller('FooterController', ['$scope','$location','$anchorScroll',function($scope,$location,$anchorScroll) {
+    $scope.footerVisible = false;
+    $scope.goToTop = function() {
+      var old = $location.hash();
+      $location.hash('pagebody');
+      $anchorScroll();
+      //workaround to avoid ngroute interaction in a simple way
+      $location.hash(old);
+    }
+  }]).
+  directive("detectscroll", function ($window) {
+  	return function(scope, element, attrs) {
+  		angular.element($window).bind("scroll", function() {
+  			if (this.pageYOffset >= 100) {
+                 scope.footerVisible = true;
+            } else {
+                 scope.footerVisible = false;
+            }
+            scope.$apply();
+  		});
+  	}
+  });
