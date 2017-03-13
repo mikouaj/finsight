69763628ed59ee17ca3c734a6a28c96489ab646d
mikouaj <nick@surreal.pl>
2017-03-13 17:13:48 +0100

    TG-4 added functions and graphs for types and destinations

--- finsight-frontend/app/reports/category/category.component.js
+++ finsight-frontend/app/reports/category/category.component.js
@@ -17,10 +17,41 @@ angular.
       self.destChart.type = "BarChart";
       self.destChart.data={"cols": [
         {id: "t", label: "Type", type: "string"},
-        {id: "c", label: "Transactions", type: "number"}
+        {id: "s", label: "Total", type: "number"},
+        {id: "c", label: "Cnt", type: "string","p":{"role":"annotation"}}
       ], "rows": []};
+      self.destChart.options = {
+        hAxis: {
+          title: 'Total amount',
+          minValue: 0,
+        },
+        vAxis: {
+          title: 'Destination'
+        },
+        annotations: {
+          alwaysOutside: true,
+          textStyle: {
+            fontSize: 12,
+            auraColor: 'none',
+            color: '#555'
+          },
+          boxStyle: {
+            stroke: '#ccc',
+            strokeWidth: 1,
+            gradient: {
+              color1: '#f3e5f5',
+              color2: '#f3e5f5',
+              x1: '0%', y1: '0%',
+              x2: '100%', y2: '100%'
+            }
+          }
+        }
+      }
 
-      var transactionCache={};
+      self.inteliData={};
+      self.inteliData.typesCount={};
+      self.inteliData.monthCount={};
+      self.inteliData.destData={};
 
       Backend.getApi().then(function(api) {
         api.labels.get().then(function(response) {
@@ -56,6 +87,12 @@ angular.
         self.dateToPopupOpened = true;
       }
 
+      self.labelSelected = function labelSelected(item,model) {
+        self.queryReportData(self.dateFrom,self.dateTo,model.id);
+      }
+
+      // Intel related functions
+
       self.getFormattedDate = function(date) {
         var dd = date.getDate();
         if (dd<10) dd= '0'+dd;
@@ -69,7 +106,7 @@ angular.
         for(var type in typesCountData) {
           if (typesCountData.hasOwnProperty(type)) {
             rows.push(
-              {c:[{v: type},{v: typesCountData[type]},]}
+              {c:[{v: type},{v: typesCountData[type].cnt}]}
             );
           }
         }
@@ -77,52 +114,62 @@ angular.
       }
 
       self.updateDestChart = function(destCountData) {
-
         var rows=[];
         for(var dest in destCountData) {
           if (destCountData.hasOwnProperty(dest)) {
             rows.push(
-              {c:[{v: dest},{v: destCountData[dest]},]}
+              {c:[{v: dest},{v: destCountData[dest].sum},{v: "Count: "+destCountData[dest].cnt}]}
             );
           }
         }
-        self.destChart.data.rows = rows;
+        rows.sort(function(a,b) {
+          return b.c[1].v - a.c[1].v;
+        });
+        self.destChart.data.rows = rows.slice(0,5);
+      }
+
+      self.updateSum = function(map,key,amount) {
+        if(typeof map[key] === 'undefined') {
+          map[key]={sum:amount,cnt:1};
+        } else {
+          map[key].sum+=amount;
+          map[key].cnt++;
+        }
+      }
+
+      self.getTransactionMonth = function(transaction) {
+        var date = new Date(transaction.date);
+        var month = date.getMonth() +1;
+        if (month<10) month= '0'+month;
+        return month;
+      }
+
+      self.getDestForTrans = function(transaction) {
+        var dest='N/A';
+        if(transaction.type == 'CardOperation') {
+          dest = transaction.details.destination;
+        } else if(transaction.type== 'Transfer') {
+          dest = transaction.details.dstAccount.name;
+        } else if(transaction.type== 'Commission') {
+          dest = "Commission";
+        }
+        return dest;
       }
 
       self.queryReportData = function(dateFrom,dateTo,labelId) {
         Backend.getApi().then(function(api) {
           api.transactions.get({},{params:{label:labelId,dateFrom:self.getFormattedDate(dateFrom),dateTo:self.getFormattedDate(dateTo)}}).then(function(response) {
-            var typeCount={};
-            var destCount={};
             for(var id in response.data) {
               var transaction = response.data[id];
-              if(typeof typeCount[transaction.type] === 'undefined') {
-                typeCount[transaction.type]=1;
-              } else {
-                typeCount[transaction.type]++;
-              }
-              self.updateTypesChart(typeCount);
 
-              var dest='N/A';
-              if(transaction.type == 'CardOperation') {
-                dest = transaction.details.destination;
-              }
-              if(transaction.type== 'Transfer') {
-                dest = transaction.details.dstAccount.name;
-              }
-              if(typeof destCount[dest] === 'undefined') {
-                destCount[dest]=1;
-              } else {
-                destCount[dest]++;
-              }
-              self.updateDestChart(destCount);
+              self.updateSum(self.inteliData.monthCount,self.getTransactionMonth(transaction),transaction.accountingAmount);
+              self.updateSum(self.inteliData.typesCount,transaction.type,transaction.accountingAmount);
+              self.updateSum(self.inteliData.destData,self.getDestForTrans(transaction),transaction.accountingAmount);
             }
+            self.updateTypesChart(self.inteliData.typesCount);
+            self.updateDestChart(self.inteliData.destData);
           });
         });
-      }
-
-      self.labelSelected = function labelSelected(item,model) {
-        self.queryReportData(self.dateFrom,self.dateTo,model.id);
       }
     }]
   });
